42215239ffb02c5d9bd1bddcb4a573285e25cde8
Elijah <elijah.ivanik@inbox.ru>
2023-05-13 09:08:57 +0300

    feat: implement global body padding from top & bottom

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,12 +3,15 @@ import { Outlet } from "react-router";
 
 import Header from "components/Header";
 import Footer from "components/Footer";
+import { Box } from "@mui/material";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Box sx={{ pt: 10, pb: 10 }}>
+        <Outlet />
+      </Box>
       <Footer />
     </>
   );
