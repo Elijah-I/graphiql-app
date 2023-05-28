af4472f3049877bf0f9732b744f54adedb825c9f
Alex Khitry <khitrii03@gmail.com>
2023-05-28 22:03:47 +0300

    feat: add ru for register

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -8,9 +8,11 @@ import {
 } from "../../firebase/firebase";
 import "./Register.scss";
 import { FieldValues, useForm } from "react-hook-form";
+import useLanguage from "hooks/useLanguage";
 function Register() {
   const { register, handleSubmit, formState: { errors, isValid }, } = useForm({ mode: 'onSubmit', reValidateMode: 'onSubmit' });
   const [user, loading, error] = useAuthState(auth);
+  const locale = useLanguage("reg");
   async function onSubmit(data: FieldValues) {
     try {
       if(isValid) {
@@ -36,33 +38,33 @@ function Register() {
         <input
           type="text"
           className="register__textBox"
-          placeholder="Full Name"
+          placeholder={locale.name}
           {...register("name", { required: true, minLength: 8})}
         />
-        {errors.name && <span>minimum 8 symbols</span>}
+        {errors.name && <span>{locale.nameval}</span>}
         <input
           type="email"
           className="register__textBox"
-          placeholder="E-mail Address"
+          placeholder={locale.email}
           {...register("email", { required: true, minLength: 8})}
         />
-        {errors.email && <span>minimum 8 symbols</span>}
+        {errors.email && <span>{locale.loginval}</span>}
         <input
           type="password"
           className="register__textBox"
-          placeholder="Password"
+          placeholder={locale.password}
           {...register("password", {
             required: true, 
             minLength: 8, 
             validate: (value: string) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/.test(value)
           })}
         />
-        {errors.password && <span>minimum 8 symbols, at least one letter, one digit, one special character</span>}
+        {errors.password && <span>{locale.passwordval}</span>}
         <Button variant="contained" className="register__btn" type='submit'>
-          Register
+          {locale.reg}
         </Button>
         <div>
-          Already have an account? <Link style={{color: "blue"}} to="/login">Login</Link> now.
+          {locale.acc} <Link style={{color: "blue"}} to="/login">{locale.login}</Link> {locale.now}.
         </div>
       </div>
     </div>
